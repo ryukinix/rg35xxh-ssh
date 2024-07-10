8e91c447e5272970ddde377f60ee382b8669e613
Manoel Vilela <manoel_vilela@engineer.com>
2024-07-10 16:14:54 -0300

    Fix script for lang changing with non-interactive mode

--- Roms/APPS/unchained_change_language.sh
+++ Roms/APPS/unchained_change_language.sh
@@ -2,8 +2,8 @@
 echo $0 $*
 progdir=`dirname "$0"`
 
-whoami 
-
 apt-get install -y language-pack-en
-dpkg-reconfigure locales
+update-locale "LANG=en_US.UTF-8"
+locale-gen --purge "en_US.UTF-8"
+dpkg-reconfigure --frontend noninteractive locales
 update-locale LANG=en_US.UTF-8 LANGUAGE= LC_MESSAGES= LC_COLLATE= LC_CTYPE=
